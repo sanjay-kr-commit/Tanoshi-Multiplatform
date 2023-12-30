2d6d95558e55d4ff300a69e4ba70da92712c71bf
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-12-30 14:32:21 +0530

    add fieldName variable which represents button name

--- composeApp/src/commonMain/kotlin/tanoshi/multiplatform/common/extension/annotations/TAB.kt
+++ composeApp/src/commonMain/kotlin/tanoshi/multiplatform/common/extension/annotations/TAB.kt
@@ -4,4 +4,6 @@ package tanoshi.multiplatform.common.extension.annotations
 @Retention( AnnotationRetention.RUNTIME )
 
 // marked funtion will be loaded at runtime
-annotation class TAB
+annotation class TAB(
+    val fieldName : String
+)
