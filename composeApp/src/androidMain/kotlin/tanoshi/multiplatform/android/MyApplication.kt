0583df076de92569989bdda6e12000d84ff2f3ee
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-09-03 00:37:08 +0530

    request storage permission for android

--- composeApp/src/androidMain/kotlin/tanoshi/multiplatform/android/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/tanoshi/multiplatform/android/MyApplication.kt
@@ -1,16 +1,28 @@
 package tanoshi.multiplatform.android
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import android.os.Environment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
 import tanoshi.multiplatform.shared.SharedApplicationData
+
 
 class MyApplication : SharedApplicationData() {
 
-    var manageStorage : Boolean = false
+    var manageStorage : Boolean by mutableStateOf( false )
 
     override fun onCreate() {
         super.onCreate()
         extensionManager.apply {
             applicationContext = this@MyApplication.applicationContext
         }
+        manageStorage = checkForStoragePermission()
         // set uncaught exception thread
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
             handleUncaughtException( thread , throwable )
@@ -24,6 +36,15 @@ class MyApplication : SharedApplicationData() {
             throwable.stackTraceToString()
         }
         startCrashActivity()
+    }
+
+    fun checkForStoragePermission() : Boolean = when {
+        SDK_INT >= Build.VERSION_CODES.R -> Environment.isExternalStorageManager()
+        else -> {
+            val result = ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE)
+            val result1 = ContextCompat.checkSelfPermission(this , WRITE_EXTERNAL_STORAGE)
+             result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED
+        }
     }
 
     
--- composeApp/src/androidMain/kotlin/tanoshi/multiplatform/android/activities/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/tanoshi/multiplatform/android/activities/MainActivity.kt
@@ -1,19 +1,30 @@
 package tanoshi.multiplatform.android.activities
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import tanoshi.multiplatform.shared.util.ApplicationActivity
-import tanoshi.multiplatform.shared.util.setCrashActivity
+import androidx.core.app.ActivityCompat
 import tanoshi.multiplatform.common.model.MainScreenViewModel
 import tanoshi.multiplatform.common.screens.MainScreen
+import tanoshi.multiplatform.shared.util.ApplicationActivity
+import tanoshi.multiplatform.shared.util.setCrashActivity
 
 class MainActivity : ApplicationActivity() {
 
@@ -22,13 +33,58 @@ class MainActivity : ApplicationActivity() {
         setCrashActivity = CrashHandlingActivity::class.java
         val mainScreenViewModel by viewModels<MainScreenViewModel>()
         setContent {
-            Column {
-                Spacer( Modifier.height( 20.dp ) )
-                MainScreen(
-                    sharedApplicationData ,
-                    mainScreenViewModel
-                )
+            when {
+                !sharedApplicationData.manageStorage -> RequestStoragePermission()
+                else -> Column {
+                    Spacer( Modifier.height( 20.dp ) )
+                    MainScreen(
+                        sharedApplicationData ,
+                        mainScreenViewModel
+                    )
+                }
             }
+        }
+    }
+
+    @Composable
+    fun RequestStoragePermission() {
+        Box( modifier = Modifier.fillMaxSize() , contentAlignment = Alignment.Center ) {
+            Button( onClick = {
+                requestPermission()
+                sharedApplicationData.manageStorage = sharedApplicationData.checkForStoragePermission()
+            } ) {
+                Text( "Storage Permission Required" )
+            }
+        }
+
+    }
+
+    @Suppress("DEPRECATION")
+    private fun requestPermission() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                val intent: Intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                intent.addCategory("android.intent.category.DEFAULT")
+                intent.setData(
+                    Uri.parse(
+                        String.format(
+                            "package:%s",
+                            applicationContext.packageName
+                        )
+                    )
+                )
+                startActivityForResult(intent, 2296)
+            } catch (e: Exception) {
+                val intent = Intent()
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                startActivityForResult(intent, 2296)
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    WRITE_EXTERNAL_STORAGE
+                ), 0
+            )
         }
     }
 
