7ddec8ed41be469f55ebb0af02eb33e56e91594b
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-09-06 04:41:01 +0530

    make toast window look decent and move toast code to application function to avoid code clutter in main function

--- composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/desktop/MyApplication.kt
+++ composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/desktop/MyApplication.kt
@@ -1,24 +1,10 @@
 @file: JvmName( "MyApplication" )
 package tanoshi.multiplatform.desktop
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import tanoshi.multiplatform.common.screens.LogScreen
 import tanoshi.multiplatform.desktop.util.WindowStack
@@ -30,7 +16,6 @@ fun main() : Unit = SharedApplicationData(
 ).run {
 
     val windowStack = WindowStack( App() , this )
-    val toastWindowState = WindowState()
 
     customApplication( this ) {
 
@@ -38,28 +23,7 @@ fun main() : Unit = SharedApplicationData(
             windowStack.render()
         }
 
-        // show toast
-        Window( onCloseRequest = ::exitApplication ,
-            title = "Tanoshi Toast",
-            state = toastWindowState,
-            resizable = false ,
-            alwaysOnTop = true ,
-            undecorated = true ,
-            transparent = true ,
-            visible = isToastWindowVisible
-        ) {
-            AnimatedVisibility( isToastWindowVisible , enter = fadeIn() , exit = fadeOut() ) {
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(10.dp))
-                        .background(Color.White)
-                        .padding(5.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(toastMessage)
-                }
-            }
-        }
+
     }
 
     error?.let {
--- composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/desktop/util/component.kt
+++ composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/desktop/util/component.kt
@@ -1,10 +1,27 @@
 package tanoshi.multiplatform.desktop.util
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import tanoshi.multiplatform.shared.SharedApplicationData
+import java.awt.Toolkit
 import java.awt.event.WindowEvent
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -35,8 +52,45 @@ fun customApplication(
 ) = application(
     exitProcessOnExit = false
 ) {
+
+    val toastWindowState = WindowState()
+    val screenSize = Toolkit.getDefaultToolkit().screenSize
+    val screenHeight = screenSize.height
+    val screenWidth = screenSize.width
+    toastWindowState.placement = WindowPlacement.Floating
+    toastWindowState.position = WindowPosition( Alignment.BottomCenter )
+    toastWindowState.size = DpSize( screenWidth.dp , 30.dp )
+
     sharedApplicationData.applicationScope = this
+
     redirectErrors( sharedApplicationData ) {
         content()
+        // show toast
+        Window( onCloseRequest = ::exitApplication ,
+            title = "Tanoshi Toast",
+            state = toastWindowState,
+            resizable = false ,
+            alwaysOnTop = true ,
+            undecorated = true ,
+            transparent = true ,
+            visible = sharedApplicationData.isToastWindowVisible
+        ) {
+            AnimatedVisibility( sharedApplicationData.isToastWindowVisible , enter = fadeIn() , exit = fadeOut() ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding( horizontal = 5.dp ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box( modifier = Modifier.wrapContentSize()
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.White)
+                        .padding( 5.dp )
+                    ) {
+                        Text(sharedApplicationData.toastMessage)
+                    }
+                }
+            }
+        }
     }
 }
