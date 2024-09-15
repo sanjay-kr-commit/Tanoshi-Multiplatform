8ab2c5e8d00d5de91659ace2f3aab682ec220b21
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-09-15 22:54:22 +0530

    Animate Activity Change

--- composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/desktop/util/WindowStack.kt
+++ composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/desktop/util/WindowStack.kt
@@ -1,10 +1,12 @@
 package tanoshi.multiplatform.desktop.util
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -54,9 +56,13 @@ class WindowStack(
 
     @Composable
     fun render() {
-        if ( _activeWindow.value.isComposableViewSet ) _activeWindow.value.composableActivityView()
-        else Box( Modifier.fillMaxSize() , contentAlignment = Alignment.Center ) {
-            Text( "Set Composable To using setComposableContent function inside onCreate" )
+        stack.forEach { activity ->
+            AnimatedVisibility( activity == _activeWindow.value , modifier = Modifier.fillMaxSize() , enter = fadeIn() , exit = fadeOut() ) {
+                if (_activeWindow.value.isComposableViewSet) _activeWindow.value.composableActivityView()
+                else Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("Set Composable To using setComposableContent function inside onCreate")
+                }
+            }
         }
     }
 
