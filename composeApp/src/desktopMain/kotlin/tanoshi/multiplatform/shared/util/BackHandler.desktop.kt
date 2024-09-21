c278c4d0808eaffa9b3bffb99f521c7d19251f79
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-09-22 04:01:05 +0530

    use singleton to handle backStack for desktop so only one go back button is shown at a time

--- composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/shared/util/BackHandler.desktop.kt
+++ composeApp/src/desktopMain/kotlin/tanoshi/multiplatform/shared/util/BackHandler.desktop.kt
@@ -10,29 +10,62 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import java.util.*
+
+private object BackHandlerBackStack {
+    private var _unique_id_ = Int.MIN_VALUE
+
+    var activeValue by mutableStateOf( _unique_id_ )
+    val stack = Stack<Int>()
+    val attach : Int
+    get() {
+        activeValue = _unique_id_
+        stack.add( _unique_id_ )
+        return _unique_id_++
+    }
+    fun detach( uniqueId : Int ) {
+        stack.remove( uniqueId )
+        _unique_id_ = stack.peek()
+        activeValue = _unique_id_
+    }
+
+    init {
+        activeValue = _unique_id_
+        stack.add( _unique_id_ )
+        _unique_id_++
+    }
+
+}
 
 @Composable
 actual fun BackHandler(enabled: Boolean , onBack: () -> Unit) {
-    AnimatedVisibility( enabled , enter = slideIn( initialOffset = {
-        IntOffset(
-            0,
-            it.height+1
-        )
-    } ) , exit = slideOut( targetOffset = {
-        IntOffset(
-            0,
-            it.height+1
-        )
-    } ) ) {
-        Box( Modifier.padding( 5.dp ).wrapContentSize().clip( RoundedCornerShape( 10.dp ) ).clickable( onClick = onBack ).background( Color.Gray ).padding( 5.dp ) , contentAlignment = Alignment.Center ) {
-            Text( "Go Back" , color = Color.White )
+    AnimatedVisibility(
+        enabled ,
+        enter = slideIn( initialOffset = {IntOffset(0,it.height+1 )} ) ,
+        exit = slideOut( targetOffset = {IntOffset(0,it.height+1 )} )
+    ) {
+        val uniqueId by remember { mutableStateOf( BackHandlerBackStack.attach ) }
+        AnimatedVisibility(
+            uniqueId == BackHandlerBackStack.activeValue ,
+            enter = slideIn( initialOffset = {IntOffset(0,it.height+1 )} ) ,
+            exit = slideOut( targetOffset = {IntOffset(0,it.height+1 )} )
+        ) {
+            Box( Modifier.padding( 5.dp ).wrapContentSize().clip( RoundedCornerShape( 10.dp ) ).clickable( onClick = onBack ).background( Color.Gray ).padding( 5.dp ) , contentAlignment = Alignment.Center ) {
+                Text( "Go Back" , color = Color.White )
+            }
         }
+         DisposableEffect( Unit ) {
+             onDispose {
+                 BackHandlerBackStack.detach( uniqueId )
+
+             }
+         }
     }
 }
